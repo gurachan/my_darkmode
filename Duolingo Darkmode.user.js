0e3d6331d7bf2c19fbcd8b0825d3df11f3948922
gurachan <djjom2x@gmail.com>
2022-01-03 21:50:10 +0800

    Update Duolingo Darkmode.user.js

--- Duolingo Darkmode.user.js
+++ Duolingo Darkmode.user.js
@@ -2,21 +2,37 @@
 // @name         Duolingo Darkmode by gurachan
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  Darkmode?
+// @author       gurachan
 // @match        https://www.duolingo.com/*
 // @icon         https://www.google.com/s2/favicons?domain=duolingo.com
 // @grant        GM_addStyle
 // ==/UserScript==
 window.addEventListener('load', function () {
   GM_addStyle(`
-body {
-	background: #202124 !important;
-    color: whitesmoke;
+::-webkit-scrollbar {
+	width: 12px;
 }
 
-h2,h3,h4 {
-    color: #f5efef;
+::-webkit-scrollbar-track {
+	-webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+	border-radius: 10px;
+}
+
+::-webkit-scrollbar-thumb {
+	border-radius: 10px;
+	-webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
+}
+
+body {
+	background: #202124 !important;
+	color: whitesmoke;
+}
+
+h2,
+h3,
+h4 {
+	color: #f5efef;
 }
 
 ._2PVaI {
@@ -155,7 +171,7 @@ span._13HXc {
 ._2NolF:disabled:not(._1rl91):before {
 	background-color: #1cb0f6 !important;
 	cursor: pointer !important;
-    box-shadow: 0 var(--__internal__lip-width) 0 #179cdb !important;
+	box-shadow: 0 var(--__internal__lip-width) 0 #179cdb !important;
 }
 
 ._15J0U {
@@ -201,194 +217,193 @@ span._26kJ0._2FR4x {
 }
 
 .SSzTP .twkSI {
-    color: #d916c1 !important;
+	color: #d916c1 !important;
 }
 
 ._26QYy:before {
-    background: darkgoldenrod !important;
-    box-shadow: 0 var(--__internal__lip-width) 0 #a75329 !important;
+	background: darkgoldenrod !important;
+	box-shadow: 0 var(--__internal__lip-width) 0 #a75329 !important;
 }
 
 ._3OTNX {
-    background-color: rgb(78 78 78) !important;
-    border: 2px solid rgb(99 50 148) !important;
+	background-color: rgb(78 78 78) !important;
+	border: 2px solid rgb(99 50 148) !important;
 }
 
 ._2YmyD {
-    background: rgb(143 137 189) !important
+	background: rgb(143 137 189) !important
 }
 
 ._2nAgU {
-    color: white !important;
+	color: white !important;
 }
 
 ._3jfHY>._3mxWC {
-    color: whitesmoke !important;
+	color: whitesmoke !important;
 }
 
 ._3jfHY>._2s4Lh {
-    color: #d092ff !important;
+	color: #d092ff !important;
 }
 
 ._1lgsd {
-    background-color: #303134 !important;
+	background-color: #303134 !important;
 }
 
-._1qdLM:after, ._1qdLM:before {
-    background: none !important;
+._1qdLM:after,
+._1qdLM:before {
+	background: none !important;
 }
 
 ._1zsgz {
-    background-color: #303134 !important;
+	background-color: #303134 !important;
 }
 
 ._1lJ31 {
-    color: whitesmoke !important;
+	color: whitesmoke !important;
 }
 
 ._1qdLM {
-    color: whitesmoke !important;
-    padding-top: 24px !important;
+	color: whitesmoke !important;
+	padding-top: 24px !important;
 }
 
 ._2zN81 {
-    color: whitesmoke !important;
+	color: whitesmoke !important;
 }
 
 ._2nLk_ {
-    background-color: #663399 !important;
+	background-color: #663399 !important;
 }
 
 ._1eZU8 {
-    color: whitesmoke !important;
+	color: whitesmoke !important;
 }
 
 ._3tLz5 {
-    color: gold !important;
+	color: gold !important;
 }
 
 ._3kz3Z:hover {
-    background-color: #323232 !important;
+	background-color: #323232 !important;
 }
 
 ._3kz3Z {
-    border-bottom: none !important;
+	border-bottom: none !important;
 }
 
 ._1YdRX {
-    color: whitesmoke !important;
+	color: whitesmoke !important;
 }
 
 ._1KUxv {
-    border-radius: 8px !important;
+	border-radius: 8px !important;
 }
 
 ._2uf-t h2 {
-    color: whitesmoke !important;
+	color: whitesmoke !important;
 }
 
 ._2uf-t p {
-    color: #ffa0a0 !important;
+	color: #ffa0a0 !important;
 }
 
 ._3uAwj {
-    color: #ffa0a0 !important;
+	color: #ffa0a0 !important;
 }
 
 ._3PrAS {
-    color: whitesmoke !important;
+	color: whitesmoke !important;
 }
 
 .uqCpu ._2WiQc:hover {
-    background-color: #323232 !important;
-    color: whitesmoke !important;
+	background-color: #323232 !important;
+	color: whitesmoke !important;
 }
 
 .uqCpu .uOkpe {
-    border: none !important;
+	border: none !important;
 }
 
 .uqCpu ._1Gh9e {
-    background-color: #202124 !important;
+	background-color: #202124 !important;
 }
 
 .uqCpu .uOkpe:hover {
-    background-color: #323232 !important;
+	background-color: #323232 !important;
 }
 
 ._2WiQc._1Gh9e {
-    color: whitesmoke !important;
+	color: whitesmoke !important;
 }
 
 ._11VOS {
-    background: #202124 !important;
-    border-top: none !important;
+	background: #202124 !important;
+	border-top: none !important;
 }
 
 .xzblA {
-    background-color: #202124 !important;
+	background-color: #202124 !important;
 }
 
 ._2igzU._3LUrt {
-    color: #c790ff !important;
+	color: #c790ff !important;
 }
 
 ._2QKoe {
-    background-color: #202124 !important;
+	background-color: #202124 !important;
 }
 
-._6tX_H{
-   background-color: #202124 !important;
+._6tX_H {
+	background-color: #202124 !important;
 }
 
-._3jHvQ{
-   background-color: #202124 !important;
+._3jHvQ {
+	background-color: #202124 !important;
 }
 
 .saQLX {
-    color: whitesmoke !important;
+	color: whitesmoke !important;
 }
 
 .SBf4a {
-   background: #202124 !important;
+	background: #202124 !important;
 }
 
 ._3oNvD {
-   background: #202124 !important;
+	background: #202124 !important;
 }
 
 ._1jKLW {
-   background: #202124 !important;
+	background: #202124 !important;
 }
 
 ._2GPX6 {
-   background: #202124 !important;
+	background: #202124 !important;
 }
 
 .hOVdl {
-    color: #f9aeae !important;
+	color: #f9aeae !important;
 }
 
 ._1VA3z {
-    color: mediumpurple !important;
+	color: mediumpurple !important;
 }
 
 ._3ZuGY {
-    background: rebeccapurple !important;
+	background: rebeccapurple !important;
 }
 
 ._1wy04 {
-    color: whitesmoke !important;
+	color: whitesmoke !important;
 }
 
 ._2cQin {
-  color: whitesmoke  !important;
+	color: whitesmoke !important;
 }
 
 ._2IZtO {
-  color: #e59e9e  !important;
+	color: #e59e9e !important;
 }
-
     `);
 })
-
